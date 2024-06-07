7ed4cc141a02228edece6728954f88efcd244276
Liza <desseobscurite@gmail.com>
2024-06-07 19:07:47 +0400

    Created Model

---
+++ NewsApp/Articles.swift
@@ -0,0 +1,38 @@
+//
+//  Articles.swift
+//  NewsApp
+//
+//  Created by Elizaveta Medvedeva on 07.06.24.
+//
+
+import Foundation
+
+struct NewsResponse: Decodable {
+    let status: String?
+    let totalResults: Int?
+    let articles: [Article]
+}
+
+struct Article: Decodable, Identifiable {
+    var id = UUID()
+    let title: String
+    let description: String?
+    let author: String?
+    let urlToImage: String?
+    let publishedAt: Data?
+    let source: Source
+}
+
+struct SourceResponse: Decodable {
+    let status: String
+    let sources: [Source]
+}
+
+struct Source: Decodable, Identifiable {
+    let id: String?
+    let name: String?
+    let description: String?
+    let country: String?
+    let category: String?
+    let url: String?
+}
