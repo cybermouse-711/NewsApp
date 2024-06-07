86cc40185543c8b236ac778ca026f99d0721715a
Liza <desseobscurite@gmail.com>
2024-06-07 18:22:21 +0400

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Elizaveta Medvedeva on 07.06.24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
