de6ebcd9e3c172c6b515fe676b905c11e0ba2949
Liza <desseobscurite@gmail.com>
2024-07-05 18:57:29 +0400

    Added property ArticlesViewModelError in struct ContentViewArticles

--- NewsApp/Views/ContentViewArticles.swift
+++ NewsApp/Views/ContentViewArticles.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 
 struct ContentViewArticles: View {
-    @ObservedObject var articlesViewModel = ArticlesViewModel()
+    @ObservedObject var articlesViewModelError = ArticlesViewModelError()
     
     var body: some View {
         VStack {
-            Picker("", selection: $articlesViewModel.indexEndpoint) {
+            Picker("", selection: $articlesViewModelError.indexEndpoint) {
                 Text("topHeadLines").tag(0)
                 Text("search").tag(1)
                 Text("from category").tag(2)
             }.pickerStyle(SegmentedPickerStyle())
-            if articlesViewModel.indexEndpoint == 1 {
-                SearchView(searchTerm: $articlesViewModel.searchString)
+            if articlesViewModelError.indexEndpoint == 1 {
+                SearchView(searchTerm: $articlesViewModelError.searchString)
             }
-            if articlesViewModel.indexEndpoint == 2 {
-                Picker("", selection: $articlesViewModel.searchString) {
+            if articlesViewModelError.indexEndpoint == 2 {
+                Picker("", selection: $articlesViewModelError.searchString) {
                     Text("sports").tag("sports")
                     Text("health").tag("health")
                     Text("science").tag("science")
@@ -29,10 +29,16 @@ struct ContentViewArticles: View {
                     Text("technology").tag("technology")
                 }
                 .onAppear(perform: {
-                    self.articlesViewModel.searchString = "science"
+                    self.articlesViewModelError.searchString = "science"
                 }).pickerStyle(SegmentedPickerStyle())
             }
-            ArticlesList(articles: articlesViewModel.articles)
+            ArticlesList(articles: articlesViewModelError.articles)
+        }
+        .alert(item: self.$articlesViewModelError.articlesError) { error in
+            Alert(
+                title: Text("Network Error"),
+                message: Text(error.localizedDescription).font(.subheadline),
+                dismissButton: .default(Text("OK")))
         }
     }
 }
